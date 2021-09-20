5805a69a4ffd3859b323046ee5603b384a4acc9e
Caio Teixeira <caio.xd63@hotmail.com>
2021-09-20 12:03:47 -0300

    feat: add a layer that processes Wyre's API responses

---
+++ polaris_wyre/wyre/wyre.py
@@ -0,0 +1,32 @@
+from typing import Tuple
+
+from polaris_wyre.wyre.dtos import TransferData
+from .api import TEST_BASE_URL, WyreAPI
+
+
+# TODO: add methods description
+class Wyre:
+    def __init__(
+        self, api_token: str = "", account_id: str = "", api_url: str = TEST_BASE_URL
+    ):
+        self.wyre_api = WyreAPI(
+            api_token=api_token, account_id=account_id, api_url=api_url
+        )
+
+    def get_account(self) -> Tuple[str, str]:
+        response_data = self.wyre_api.get_account()
+        return response_data["depositAddresses"]["XLM"].split(":")
+
+    def get_stellar_transaction_id(self, transfer_id) -> str:
+        transfer_is_completed = False
+        while not transfer_is_completed:
+            response_data = self.wyre_api.get_transfer_by_id(transfer_id)
+            if response_data["status"] == "FAILED":
+                # TODO: improve RuntimeError message with a better description
+                raise RuntimeError("Wyre failed to complete the transfer.")
+            if response_data["status"] == "COMPLETED":
+                transfer_is_completed = True
+        return response_data["blockchainTx"]["networkTxId"]
+
+    def create_transfer(self, transfer_data: TransferData) -> dict:
+        return self.wyre_api.create_transfer(transfer_data)
--- tests/test_wyre_api.py
+++ tests/test_wyre_api.py
@@ -1,16 +1,12 @@
-from decimal import Decimal
 from urllib.parse import urljoin
 
 import pytest
 from django.conf import settings
 from rest_framework import status
-from stellar_sdk import Keypair
 
 from polaris_wyre.helpers.exceptions import WyreAPIError
 from polaris_wyre.wyre.api import TEST_BASE_URL
-from polaris_wyre.wyre.dtos import TransferData
 from .mocks import wyre as wyre_mocks
-
 
 REQUEST_METHOD_GET_MOCK = "requests.Session.get"
 REQUEST_METHOD_POST_MOCK = "requests.Session.post"
@@ -106,34 +102,26 @@ def test_get_transfer_by_id_unauthorized(mocker, make_wyre_api):
     wyre_request_mock.assert_called_once_with(url)
 
 
-def test_create_transfer_success(mocker, make_wyre_api):
-    currency = "USDC"
-    amount = Decimal("10")
-    destination = f"stellar:{Keypair.random().public_key}"
-
-    transfer_data = TransferData.create_transfer_data(
-        currency=currency,
-        amount=amount,
-        destination=destination,
-    )
+def test_create_transfer_success(mocker, make_wyre_api, make_transfer_data):
+    transfer_data = make_transfer_data()
 
     wyre_request_mock = mocker.patch(
         REQUEST_METHOD_POST_MOCK,
         return_value=wyre_mocks.create_transfer_response(
             account_id=settings.WYRE_ACCOUNT_ID,
-            currency=currency,
-            amount=amount,
-            destination=destination,
+            currency=transfer_data.currency,
+            amount=transfer_data.amount,
+            destination=transfer_data.destination,
         ),
     )
 
     data = {
         "autoConfirm": True,
         "source": f"account:{settings.WYRE_ACCOUNT_ID}",
-        "sourceCurrency": currency,
-        "sourceAmount": str(amount),
-        "dest": destination,
-        "destCurrency": currency,
+        "sourceCurrency": transfer_data.currency,
+        "sourceAmount": str(transfer_data.amount),
+        "dest": transfer_data.destination,
+        "destCurrency": transfer_data.currency,
     }
 
     wyre_api = make_wyre_api()
@@ -145,29 +133,23 @@ def test_create_transfer_success(mocker, make_wyre_api):
 
     assert response == wyre_request_mock.return_value.json()
     assert response["source"] == f"account:{settings.WYRE_ACCOUNT_ID}"
-    assert response["destAmount"] == amount
-    assert response["dest"] == destination
-    assert response["destCurrency"] == currency == response["sourceCurrency"]
-
-
-def test_create_transfer_bad_request(mocker, make_wyre_api):
-    currency = "USDC"
-    amount = Decimal("10")
-    destination = f"stellar:{Keypair.random().public_key}"
-
-    transfer_data = TransferData.create_transfer_data(
-        currency=currency,
-        amount=amount,
-        destination=destination,
+    assert response["destAmount"] == transfer_data.amount
+    assert response["dest"] == transfer_data.destination
+    assert (
+        response["destCurrency"] == transfer_data.currency == response["sourceCurrency"]
     )
+
+
+def test_create_transfer_bad_request(mocker, make_wyre_api, make_transfer_data):
+    transfer_data = make_transfer_data()
 
     data = {
         "autoConfirm": True,
         "source": f"account:{settings.WYRE_ACCOUNT_ID}",
-        "sourceCurrency": currency,
-        "sourceAmount": str(amount),
-        "dest": destination,
-        "destCurrency": currency,
+        "sourceCurrency": transfer_data.currency,
+        "sourceAmount": str(transfer_data.amount),
+        "dest": transfer_data.destination,
+        "destCurrency": transfer_data.currency,
     }
 
     url = urljoin(TEST_BASE_URL, "v3/transfers")
@@ -175,9 +157,9 @@ def test_create_transfer_bad_request(mocker, make_wyre_api):
         REQUEST_METHOD_POST_MOCK,
         return_value=wyre_mocks.create_transfer_response(
             account_id=settings.WYRE_ACCOUNT_ID,
-            currency=currency,
-            amount=amount,
-            destination=destination,
+            currency=transfer_data.currency,
+            amount=transfer_data.amount,
+            destination=transfer_data.destination,
             status_code=status.HTTP_400_BAD_REQUEST,
             url=url,
             reason="Bad Request",
